eb9e6f8259bc84388beba33d886a31721e10c19f
Max Maximov <max.maximov@gmail.com>
2015-05-16 23:10:26 +0300

    server

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
-var express = require('express');
+#!/usr/bin/env node
+
+var http = require('http');
 var path = require('path');
-var logger = require('morgan');
+var express = require('express');
+var debug = require('debug')('my-user-grid:server');
 
 var app = express();
 
-app.use(logger('dev'));
+var port = process.env.PORT || '3000';
+app.set('port', port);
+
+var server = http.createServer(app);
+server.listen(port);
+
+server.on('error', function (error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = 'Port ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+});
+
+server.on('listening', function () {
+  var addr = server.address();
+  var bind = 'port ' + addr.port;
+  debug('Listening on ' + bind);
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(err, req, res, next) {
