77ad89780fbe0eace011cbf1ce44dac7c418758a
Max Maximov <max.maximov@gmail.com>
2015-05-17 03:41:41 +0300

    demo ajax

--- public/demo/demo.js
+++ public/demo/demo.js
@@ -3,7 +3,3 @@
 require('../my-user-grid/my-user-grid');
 
 var app = angular.module('demo', ['myUserGrid']);
-
-$.get('/demo/data/mock.json', function (data) {
-    localStorage.setItem('my-data', JSON.stringify(data));
-});
